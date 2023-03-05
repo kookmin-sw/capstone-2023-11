dd0bec3bf2c4c932aa6a033bc22531b733087c11
hayoun <fishingest@kookmin.ac.kr>
2023-03-06 01:36:53 +0900

    [feat] 폰트 설정 및 라이브러리 세팅

--- web_frontend/src/styles/global.tsx
+++ web_frontend/src/styles/global.tsx
@@ -11,7 +11,18 @@ const GlobalStyle = createGlobalStyle`
     font-size: 62.5%;
 
   }
-
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+  @font-face {
+    font-family: 'Pretendard-Bold';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
+}
 
   * {
     box-sizing: border-box;
