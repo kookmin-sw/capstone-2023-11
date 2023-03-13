8650bcd085995010541dce751527bf4abe2c20bb
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-13 21:07:58 +0900

    [feat] userWard 엔티티 구현

--- backend/server/src/main/java/capstone/server/utils/BaseTimeEntity.java
+++ backend/server/src/main/java/capstone/server/utils/BaseTimeEntity.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -17,9 +18,9 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createdDate;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime modifiedDate;
+    private LocalDateTime modifiedAt;
 }
