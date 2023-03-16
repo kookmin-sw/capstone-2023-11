4813c9a317b5dd8499947d6a0ec8834473812efd
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2023-03-16 19:59:14 +0900

    [feat] 뒤로가기 버튼 컴포넌트 제작

---
+++ web_frontend/src/components/common/BackButton.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StButtonBack = styled.img`
+  width: 2rem;
+  height: 2rem;
+  margin: 1rem;
+`;
+const BackButton = (props: { to: string }) => {
+  return (
+    <StButtonBack src={require("../../assets/images/img_left.png")}>
+      <Link to={props.to}></Link>
+    </StButtonBack>
+  );
+};
+
+export default BackButton;
