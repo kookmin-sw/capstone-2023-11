cfb9ad93829e6efdc2ecd937126d0b3f0496c67a
CULRRY <rkaghwns8@naver.com>
2023-03-21 01:36:42 +0900

    [feat] MedicineRepository jpa연결

--- backend/server/src/main/java/capstone/server/medicine/repository/MedicineRepository.java
+++ backend/server/src/main/java/capstone/server/medicine/repository/MedicineRepository.java
@@ -1,4 +1,9 @@
 package capstone.server.medicine.repository;
 
-public class MedicineRepository {
+import capstone.server.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 }
