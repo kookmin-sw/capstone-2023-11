2eda604ff86853ef10be8fd0561cda479b0c13da
CULRRY <rkaghwns8@naver.com>
2023-03-21 01:35:59 +0900

    [feat] dto 용도에 맞게 다시 생성

---
+++ backend/server/src/main/java/capstone/server/medicine/dto/BarcodeDto.java
@@ -0,0 +1,12 @@
+package capstone.server.medicine.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BarcodeDto {
+    private String name;
+}
