1800d83ac2f06457339236f2ce46704f4ce7161c
CULRRY <rkaghwns8@naver.com>
2023-03-21 01:35:39 +0900

    [feat] MedicineController 바코드로 약 등록, 처방전으로 약 등록 분리 구현

--- backend/server/src/main/java/capstone/server/medicine/controller/MedicineController.java
+++ backend/server/src/main/java/capstone/server/medicine/controller/MedicineController.java
@@ -1,4 +1,31 @@
 package capstone.server.medicine.controller;
 
+import capstone.server.medicine.dto.BarcodeDto;
+import capstone.server.medicine.dto.PrescriptionDto;
+import capstone.server.medicine.service.MedicineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MedicineController {
+
+    @Autowired
+    private MedicineService medicineService;
+
+    @PostMapping(value = "/medicine/barcode")
+    public ResponseEntity<?> registerMedicineByBarcode(@RequestBody BarcodeDto barcodeDto) {
+        medicineService.registerMedicineByBarcode(barcodeDto);
+        return ResponseEntity.ok().body("Success");
+    }
+
+    @PostMapping(value = "/medicine/prescription")
+    public ResponseEntity<?> registerMedicineByPrescription(@RequestBody PrescriptionDto prescriptionDto) {
+        medicineService.registerMedicineByPrescription(prescriptionDto);
+        return ResponseEntity.ok().body("Success");
+    }
 }
