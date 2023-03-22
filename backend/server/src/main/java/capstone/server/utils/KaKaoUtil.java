834fd4f4e472bb04055acfbab78640b526d6b624
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-23 01:53:30 +0900

    [feat] 카카오 URL을 위한 유틸 클래스

---
+++ backend/server/src/main/java/capstone/server/utils/KaKaoUtil.java
@@ -0,0 +1,6 @@
+package capstone.server.utils;
+
+public class KaKaoUtil {
+  public static String KakaoUserInfoURLWithAccessToken = "https://kapi.kakao.com/v2/user/me";
+  public static String KakaoUserInfoURLWithAccessTokenSecureTrue = "https://kapi.kakao.com/v2/user/me?secure_resource=true";
+}
