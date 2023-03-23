24739ac17c87b9f47e8ca52513d1c1c1fc718772
hayoun <fishingest@kookmin.ac.kr>
2023-03-23 15:08:35 +0900

    [feat] 노인 assets svg 추가

--- web_frontend/src/assets/icons/index.ts
+++ web_frontend/src/assets/icons/index.ts
@@ -1,1 +1,3 @@
 export { default as KakaoLogin } from "../icons/icn_kakaoLogin.svg";
+export { default as GrandFather } from "../icons/icn_grandfather.svg";
+export { default as GrandMother } from "../icons/icn_grandmother.svg";
