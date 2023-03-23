a91b37b5148300460417257da455b59134d34527
동재 <59719500+jea0716@users.noreply.github.com>
2023-03-23 14:09:49 +0900

    [fix] 수정

--- web_frontend/src/components/SeniorMain.tsx
+++ web_frontend/src/components/SeniorMain.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Pill from "./seniorSchedule/Pill";
+import Pill from "./Pill";
 
 function SeniorMai() {
   return (
@@ -7,7 +7,7 @@ function SeniorMai() {
       <StHeader>
         <StUser>아이콘</StUser>
       </StHeader>
-      <StMain>
+      <MenuList>
         <StMainItem>
           <Pill />
         </StMainItem>
@@ -15,7 +15,7 @@ function SeniorMai() {
         <StAIDoc>AI</StAIDoc>
         <StFood>음식</StFood>
         <StExercise>운동</StExercise>
-      </StMain>
+      </MenuList>
     </>
   );
 }
@@ -37,9 +37,24 @@ const StUser = styled.div`
   }
 `;
 
-const StMain = styled.div`
-  font-size: 2rem;
-`;
+//const TopItem = styled.div``;
+
+const MenuList = styled.ul``;
+
+// const MenuItem = styled.li`
+//   background-color: white;
+//   width: 100%;
+//   color: black;
+//   display: inline-flex;
+//   margin-left: 1rem;
+//   margin-bottom: 3rem;
+//   margin-top: 3rem;
+//   border-radius: 0.5rem;
+//   font-size: 2rem;
+//   &:hover {
+//     color: gray;
+//   }
+// `;
 
 const StMainItem = styled.div`
   display: flex;
--- web_frontend/src/core/router.tsx
+++ web_frontend/src/core/router.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SeniorMypage from "../pages/SeniorMypage";
 import SeniorSchedule from "../components/seniorSchedule/seniorSchedule";
 import SeniorMain from "../components/SeniorMain";
+import SeniorSummaryPage from "../pages/SeniorSummaryPage";
+import SeniorMealCheckPage from "../pages/SeniorMealCheckPage";
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/senior/myPage" element={<SeniorMypage />} />
         <Route path="/schedule" element={<SeniorSchedule />} />
+        <Route path="/summary" element={<SeniorSummaryPage />} />
+        <Route path="/senior/MealCheck" element={<SeniorMealCheckPage />} />
         <Route path="/senior/main" element={<SeniorMain />} />
       </Routes>
     </BrowserRouter>
