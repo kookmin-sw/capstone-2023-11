3bfa3552d1ca68fb597f72c1a5a41fb40bbf94da
hayoun <fishingest@kookmin.ac.kr>
2023-03-24 00:49:30 +0900

    [feat] 호출 쿼리 설정

--- web_frontend/src/pages/KakaoAuthPage.tsx
+++ web_frontend/src/pages/KakaoAuthPage.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
-import { getKakaoAccessToken } from "../core/api/index";
+import { getKakaoAccessToken, getUserStatus } from "../core/api/index";
 import { useEffect, useState } from "react";
 
 function KakaoAuthPage() {
@@ -10,10 +10,18 @@ function KakaoAuthPage() {
   const { data } = useQuery("accessToken", () => getKakaoAccessToken(authenticationCode), {
     enabled: !!authenticationCode,
   });
+  const { data: loginData } = useQuery(
+    "userStatus",
+    () => getUserStatus(localStorage.getItem("userStatus"), accessToken),
+    {
+      enabled: !!accessToken,
+    },
+  );
+  console.log(loginData);
   useEffect(() => {
     setAccessToken(data?.data.access_token);
-  }, [data]);
-  console.log(accessToken);
+  }, [data, accessToken]);
+
   return <></>;
 }
 export default KakaoAuthPage;
