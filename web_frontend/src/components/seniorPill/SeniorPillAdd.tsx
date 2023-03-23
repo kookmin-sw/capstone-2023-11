c0a70462c2de4f96570dec63b79a01c4d89641ad
동재 <59719500+jea0716@users.noreply.github.com>
2023-03-23 17:26:58 +0900

    [chore] 약 API 수정

--- web_frontend/src/components/seniorPill/SeniorPillAdd.tsx
+++ web_frontend/src/components/seniorPill/SeniorPillAdd.tsx
@@ -1,21 +1,27 @@
 // import { useEffect, useState } from "react";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import styled from "styled-components";
 // https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService03/getDrugPrdtPrmsnDtlInq02?serviceKey=zKSH%2F9jINWNjCG3mSkBuStun63jSwB2Ydqc3KY68unj1wo50jqvFuJBtVSv3ZIt1F12IZh9aJyXSgUzN%2BY8Y9Q%3D%3D&type=json&item_name=무코스타
 // http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?ServiceKey=zKSH%2F9jINWNjCG3mSkBuStun63jSwB2Ydqc3KY68unj1wo50jqvFuJBtVSv3ZIt1F12IZh9aJyXSgUzN%2BY8Y9Q%3D%3D&itemName=타이레놀&type=json
 function SeniorPillAdd() {
-  const [info, setInfo] = useState<PillData>();
-  useEffect(() => {
-    (async () => {
-      const infoData = await (
-        await fetch(
-          `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService03/getDrugPrdtPrmsnDtlInq02?serviceKey=zKSH%2F9jINWNjCG3mSkBuStun63jSwB2Ydqc3KY68unj1wo50jqvFuJBtVSv3ZIt1F12IZh9aJyXSgUzN%2BY8Y9Q%3D%3D&type=json&item_name=무코스타`,
-        )
-      ).json();
-      console.log(infoData);
-      setInfo(infoData);
-    })();
-  }, []);
+  // const [info, setInfo] = useState<PillData>();
+  // useEffect(() => {
+  //   (async () => {
+  //     const infoData = await (
+  //       await fetch(
+  //         `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService03/getDrugPrdtPrmsnDtlInq02?serviceKey=zKSH%2F9jINWNjCG3mSkBuStun63jSwB2Ydqc3KY68unj1wo50jqvFuJBtVSv3ZIt1F12IZh9aJyXSgUzN%2BY8Y9Q%3D%3D&type=json&item_name=무코스타&numOfRows=1`,
+  //       )
+  //     ).json();
+  //     setInfo(infoData);
+  //   })();
+  // }, []);
+  // console.log(info);
+  const { data: pillData } = useQuery<PillData>([], () =>
+    fetch(
+      `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService03/getDrugPrdtPrmsnDtlInq02?serviceKey=zKSH%2F9jINWNjCG3mSkBuStun63jSwB2Ydqc3KY68unj1wo50jqvFuJBtVSv3ZIt1F12IZh9aJyXSgUzN%2BY8Y9Q%3D%3D&type=json&item_name=무코스타`,
+    ).then((response) => response.json()),
+  );
+  console.log(pillData?.body?.items);
   return (
     <>
       <StHeader>
@@ -24,7 +30,7 @@ function SeniorPillAdd() {
       </StHeader>
       <StBody>
         <StAddList>
-          <StAddResult>{info?.name}</StAddResult>
+          <StAddResult>{pillData?.body?.items?.item?.ITEM_NAME}</StAddResult>
           <StAddItem>처방전 인식하기</StAddItem>
           <StAddItem>바코드 인식하기</StAddItem>
           <StAddItem>직접 입력하기</StAddItem>
@@ -35,7 +41,60 @@ function SeniorPillAdd() {
 }
 
 interface PillData {
-  name: string;
+  header: {
+    resultCode: "string";
+    resultMsg: "string";
+  };
+  body: {
+    numOfRows: "string";
+    pageNo: "string";
+    totalCount: "string";
+    items: {
+      item: {
+        ENTP_NO: "string";
+        MAKE_MATERIAL_FLAG: "string";
+        NEWDRUG_CLASS_NAME: "string";
+        INDUTY_TYPE: "string";
+        CANCEL_DATE: "string";
+        CANCEL_NAME: "string";
+        CHANGE_DATE: "string";
+        NARCOTIC_KIND_CODE: "string";
+        GBN_NAME: "string";
+        TOTAL_CONTENT: "string";
+        EE_DOC_DATA: "string";
+        UD_DOC_DATA: "string";
+        NB_DOC_DATA: "string";
+        PN_DOC_DATA: "string";
+        MAIN_ITEM_INGR: "string";
+        INGR_NAME: "string";
+        ATC_CODE: "string";
+        ITEM_ENG_NAME: "string";
+        ENTP_ENG_NAME: "string";
+        MAIN_INGR_ENG: "string";
+        ITEM_SEQ: "string";
+        ITEM_NAME: "string";
+        ENTP_NAME: "string";
+        ITEM_PERMIT_DATE: "string";
+        CNSGN_MANUF: "string";
+        ETC_OTC_CODE: "string";
+        CHART: "string";
+        BAR_CODE: "string";
+        MATERIAL_NAME: "string";
+        EE_DOC_ID: "string";
+        UD_DOC_ID: "string";
+        NB_DOC_ID: "string";
+        INSERT_FILE: "string";
+        STORAGE_METHOD: "string";
+        VALID_TERM: "string";
+        REEXAM_TARGET: "string";
+        REEXAM_DATE: "string";
+        PACK_UNIT: "string";
+        EDI_CODE: "string";
+        DOC_TEXT: "string";
+        PERMIT_KIND_NAME: "string";
+      };
+    };
+  };
 }
 
 const StHeader = styled.header`
