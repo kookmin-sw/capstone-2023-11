e9b9109ae817a6a65aefffd2b56d5d87e4513294
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-26 00:55:53 +0900

    [chore] 피보호자 추가 코드 입력받는 DTO로 수정

--- backend/server/src/main/java/capstone/server/domain/login/dto/GuardianJoinRequest.java
+++ backend/server/src/main/java/capstone/server/domain/login/dto/GuardianJoinRequest.java
@@ -8,13 +8,7 @@ import java.util.ArrayList;
 @Data
 @Builder
 public class GuardianJoinRequest {
-
-  private long height; // 키
-  private long weight; // 몸무게
-  private int drinkings; // 음주
-  private int smoke; // 흡연
+  private ArrayList<Long> wardCodes;
   private String kakaoAccesstoken;
-//  private ArrayList<String> ills; // 질병
-
 
 }
