168c63b2ccf5cce785bf1f90593260e19512f9c7
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-25 15:39:30 +0900

    [feat] 카카오 유틸에 인증정보로 카카오아이디와 유저타입 파싱 유틸리티 구현

--- backend/server/src/main/java/capstone/server/utils/KaKaoUtil.java
+++ backend/server/src/main/java/capstone/server/utils/KaKaoUtil.java
@@ -1,6 +1,19 @@
 package capstone.server.utils;
 
+import capstone.server.domain.login.dto.KaKaoAccountIdAndUserType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+
+@Slf4j
 public class KaKaoUtil {
   public static String KakaoUserInfoURLWithAccessToken = "https://kapi.kakao.com/v2/user/me";
   public static String KakaoUserInfoURLWithAccessTokenSecureTrue = "https://kapi.kakao.com/v2/user/me?secure_resource=true";
+
+  public static KaKaoAccountIdAndUserType authConvertIdAndTypeDto(Authentication authentication) {
+    KaKaoAccountIdAndUserType kaKaoAccountIdAndUserTypeDto = (KaKaoAccountIdAndUserType) authentication.getPrincipal();
+    log.info("userType : " + kaKaoAccountIdAndUserTypeDto.getUserType());
+    log.info("getKakaoAccountId : " +kaKaoAccountIdAndUserTypeDto.getKakaoAccountId());
+    return kaKaoAccountIdAndUserTypeDto;
+
+  }
 }
