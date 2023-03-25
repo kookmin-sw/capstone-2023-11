eddccbfc9824636944c33349551684e7163dca06
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-25 15:39:49 +0900

    [chore] 자잘한 주석 수정

--- backend/server/src/main/java/capstone/server/config/JwtFilter.java
+++ backend/server/src/main/java/capstone/server/config/JwtFilter.java
@@ -1,10 +1,9 @@
 package capstone.server.config;
 
-import capstone.server.domain.login.dto.KaKaoAccountIdAndUserTypeDto;
+import capstone.server.domain.login.dto.KaKaoAccountIdAndUserType;
 import capstone.server.domain.login.service.LoginService;
 import capstone.server.utils.JwtUtil;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -14,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -31,46 +29,46 @@ public class JwtFilter extends OncePerRequestFilter { // JWT를 계속해서 확
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    // 여기가 문이다, 인증하기전에 여기서 권한을 준다는 느낌
+	// 여기가 문이다, 인증하기전에 여기서 권한을 준다는 느낌
 
-    // 토큰이 존재 하지 않으면 인가 Block
-    final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-    log.info("authorization : ", authorization);
-    if(authorization == null || authorization.isBlank() || !authorization.startsWith("Bearer ")) {
-      log.error("authentication 정보가 잘못 됨");
-      filterChain.doFilter(request,response);
-      return;
-    }
-
-
-    // 토큰 만료일 검증
-    String token = authorization.split(" ")[1]; // Bearer 다음 값인 토큰 값 파싱
-    log.info("token : " + token);
-    // Token Expired(만료) 여부 확인
-    if (JwtUtil.isExpired(token.toString(),secretKey)) {
-      log.error("token이 만료 됨");
-      filterChain.doFilter(request,response);
-      return;
-    };
-
-    Long kakaoAccountId = JwtUtil.getKakaoAccountId(token, secretKey);
-    String userType = JwtUtil.getUserType(token, secretKey);
-    log.info("kakaoAccountId : " + kakaoAccountId);
-    log.info("userType : " + userType);
-
-    KaKaoAccountIdAndUserTypeDto kaKaoAccountIdAndUserType = KaKaoAccountIdAndUserTypeDto.builder()
-            .kakaoAccountId(kakaoAccountId)
-            .userType(userType)
-            .build();
+	// 토큰이 존재 하지 않으면 인가 Block
+	final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+	log.info("authorization : ", authorization);
+	if (authorization == null || authorization.isBlank() || !authorization.startsWith("Bearer ")) {
+	  log.error("authentication 정보가 잘못 됨");
+	  filterChain.doFilter(request, response);
+	  return;
+	}
 
 
-    // 권한 부여
-    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(kaKaoAccountIdAndUserType,null, List.of(new SimpleGrantedAuthority("USER")));
+	// 토큰 만료일 검증
+	String token = authorization.split(" ")[1]; // Bearer 다음 값인 토큰 값 파싱
+	log.info("token : " + token);
+	// Token Expired(만료) 여부 확인
+	if (JwtUtil.isExpired(token.toString(), secretKey)) {
+	  log.error("token이 만료 됨");
+	  filterChain.doFilter(request, response);
+	  return;
+	}
 
-    // 디테일을 넣어준다
-    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-    filterChain.doFilter(request,response);
+	Long kakaoAccountId = JwtUtil.getKakaoAccountId(token, secretKey);
+	String userType = JwtUtil.getUserType(token, secretKey);
+	log.info("kakaoAccountId : " + kakaoAccountId);
+	log.info("userType : " + userType);
+
+	KaKaoAccountIdAndUserType kaKaoAccountIdAndUserType = KaKaoAccountIdAndUserType.builder()
+			.kakaoAccountId(kakaoAccountId)
+			.userType(userType)
+			.build();
+
+
+	// 권한 부여
+	UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(kaKaoAccountIdAndUserType, null, List.of(new SimpleGrantedAuthority("USER")));
+
+	// 디테일을 넣어준다
+	authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+	SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+	filterChain.doFilter(request, response);
 
 
   }
