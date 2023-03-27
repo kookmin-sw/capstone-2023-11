736d4c2c7ea134bee3f1abd72611cce0516a203c
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-28 00:03:29 +0900

    [feat] 중복가입 막도록 익셉션 설정

--- backend/server/src/main/java/capstone/server/domain/login/exception/DuplicateUserException.java
+++ backend/server/src/main/java/capstone/server/domain/login/exception/DuplicateUserException.java
@@ -1,11 +1,16 @@
 package capstone.server.domain.login.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+
+@Getter
 public class DuplicateUserException extends RuntimeException{
-  public DuplicateUserException(String message) {
+  private boolean success;
+  private HttpStatus status = HttpStatus.CONFLICT;
+  public DuplicateUserException(String message,boolean success) {
 	super(message);
+	this.success = success;
   }
 }
