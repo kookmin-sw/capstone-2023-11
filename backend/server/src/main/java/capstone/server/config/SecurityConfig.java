ad976ae4c45690d3d20f2415266260e70fbdcde0
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-03-29 16:37:54 +0900

    [fix] cors WebSecurityConfigurerAdapter가 아닌 필터체인에서 해결하도록 수정
    -  WebSecurityConfigurerAdapter 디프리시에이트 됨

--- backend/server/src/main/java/capstone/server/config/SecurityConfig.java
+++ backend/server/src/main/java/capstone/server/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package capstone.server.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+public class SecurityConfig  {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
-        http
-                .authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+        configuration.addAllowedOrigin("*"); // 다른 도메인들 허용
+        configuration.addAllowedHeader("*"); // 허용하는 request header 추가
+        configuration.addAllowedMethod("*"); // GET POST PUT DELETE 메서드 허용
+        configuration.addExposedHeader("*"); // 노출할 response header 추가
+        configuration.setAllowCredentials(true); // json 자바스크립트 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
 }
