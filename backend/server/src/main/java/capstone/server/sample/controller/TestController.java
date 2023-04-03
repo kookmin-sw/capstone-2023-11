b45a60b89de07786166a7561be9e449422c05aec
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-04 00:43:45 +0900

    [feat] userward 테스트용 JWT 생성기

--- backend/server/src/main/java/capstone/server/sample/controller/TestController.java
+++ backend/server/src/main/java/capstone/server/sample/controller/TestController.java
@@ -2,17 +2,22 @@ package capstone.server.sample.controller;
 
 import capstone.server.domain.login.dto.KaKaoAccountIdAndUserType;
 import capstone.server.utils.KaKaoUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/test")
 public class TestController {
+
+  private final TestServiceImpl testService;
 
   /*
   	authentication으로 넘어오는 인자를 파서에 넣으면 id와 type이 나옴
@@ -23,4 +28,13 @@ public class TestController {
 	return ResponseEntity.ok()
 			.body(kaKaoAccountIdAndUserTypeDto.getKakaoAccountId());
   }
+
+  @GetMapping("/jwt/ward")
+  public ResponseEntity<?> jwt(){
+	String jwt = testService.temporaryJWTcreator();
+	return ResponseEntity.ok()
+			.body(jwt);
+  }
+
+
 }
