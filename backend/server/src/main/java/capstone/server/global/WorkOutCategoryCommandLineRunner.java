d16208f1a48a6014651943442923d29030b78a8c
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 01:12:44 +0900

    [feat] 서버 구동시, 카테고리 교차체크하여 빈거 있는지 확인

---
+++ backend/server/src/main/java/capstone/server/global/WorkOutCategoryCommandLineRunner.java
@@ -0,0 +1,39 @@
+package capstone.server.global;
+
+import capstone.server.domain.login.enums.MedicalCategory;
+import capstone.server.domain.medical.repository.MedicalCategoryRepository;
+import capstone.server.domain.workout.enums.WorkOutCategoryEnum;
+import capstone.server.domain.workout.repository.WorkOutCategoryRepository;
+import capstone.server.entity.MedicalHistoryCategory;
+import capstone.server.entity.WorkOutCategory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class WorkOutCategoryCommandLineRunner implements CommandLineRunner {
+
+  private final WorkOutCategoryRepository workOutCategoryRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+
+	for (WorkOutCategoryEnum item : WorkOutCategoryEnum.values()) {
+	  if (!workOutCategoryRepository.findWorkOutCategoryByName(item).isPresent()) {
+		// 카테고리가 등록 돼 있지 않을 시 등록시키기
+		workOutCategoryRepository.save(
+				WorkOutCategory.builder()
+				.name(item)
+				.kor(item.getKor())
+				.eng(item.getEng())
+				.kcalPerHour(item.getKcalPerHour())
+				.description(item.getDescription())
+				.build());
+		log.info("WorkOutCategory가 등록됐습니다 : " + item.getKor());
+	  }
+	}
+  }
+}
