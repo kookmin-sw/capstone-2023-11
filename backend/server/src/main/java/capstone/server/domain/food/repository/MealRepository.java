584f3e64fade06803ba3518c6b3c0043ad620d5c
CULRRY <rkaghwns8@naver.com>
2023-04-04 19:47:30 +0900

    [feat] getFoodInfoResponseDto 작성

--- backend/server/src/main/java/capstone/server/domain/food/repository/MealRepository.java
+++ backend/server/src/main/java/capstone/server/domain/food/repository/MealRepository.java
@@ -4,7 +4,11 @@ import capstone.server.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
     Meal findTopByUserWardUserIdOrderByCreatedAtDesc(Long userId);
+
+    List<Meal> findAllByUserWardUserId(Long userId);
 }
