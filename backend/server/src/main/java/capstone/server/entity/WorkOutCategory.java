d67e30278d3bd6296dac0881f12e124d3fbf53e9
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 02:16:57 +0900

    [chore] 운동카테고리 엔티티 toDto 추가 구현

--- backend/server/src/main/java/capstone/server/entity/WorkOutCategory.java
+++ backend/server/src/main/java/capstone/server/entity/WorkOutCategory.java
@@ -1,5 +1,6 @@
 package capstone.server.entity;
 
+import capstone.server.domain.workout.dto.WorkOutCategoryDto;
 import capstone.server.domain.workout.enums.WorkOutCategoryEnum;
 import lombok.*;
 
@@ -29,5 +30,15 @@ public class WorkOutCategory {
   private Integer kcalPerHour;
   @Column(name = "description")
   private String description;
+
+  public WorkOutCategoryDto toDto( ) {
+    return WorkOutCategoryDto.builder()
+            .kor(this.getKor())
+            .eng(this.getEng())
+            .type(this.getName())
+            .kcalPerHour(this.getKcalPerHour())
+            .description(this.getDescription())
+            .build();
+  }
 
 }
