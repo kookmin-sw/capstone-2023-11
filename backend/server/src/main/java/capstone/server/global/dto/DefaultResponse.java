03c4a8dd6b699adfeb4578b9d7574ea946556c2d
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 02:48:19 +0900

    [chore] 네이밍 변경, ErrorResponse -> DefaultResponse로 변경하여 좀더 범용성 있게 사용

--- backend/server/src/main/java/capstone/server/global/dto/DefaultResponse.java
+++ backend/server/src/main/java/capstone/server/global/dto/DefaultResponse.java
@@ -1,11 +1,10 @@
 package capstone.server.global.dto;
 
 import lombok.*;
-import org.springframework.http.HttpStatus;
 
 @Getter @Builder
 @ToString
-public class ErrorResponse {
+public class DefaultResponse {
   private int status;
   private boolean success;
   private String message;
