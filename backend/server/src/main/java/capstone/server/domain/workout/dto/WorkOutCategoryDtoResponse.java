c83a6489e1ee9d6d0c8d322a43fc6da2d1cf4cb7
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 02:18:53 +0900

    [feat] 운동 리스트 카테고리 응답 DTO

---
+++ backend/server/src/main/java/capstone/server/domain/workout/dto/WorkOutCategoryDtoResponse.java
@@ -0,0 +1,16 @@
+package capstone.server.domain.workout.dto;
+
+
+import capstone.server.domain.workout.enums.WorkOutCategoryEnum;
+import lombok.Builder;
+import lombok.Data;
+
+@Data @Builder
+public class WorkOutCategoryDtoResponse {
+  private String eng;
+  private String kor;
+  private WorkOutCategoryEnum type;
+  private Integer kcalPerHour;
+  private String description;
+
+}
