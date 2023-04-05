7f1e5c4f6b8ddc234ed647a6e0d4eddcda5476b1
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 23:28:23 +0900

    [feat] 과거 운동력 전체를 시간순 내림차순으로 조회하는 컨트롤러 구현

--- backend/server/src/main/java/capstone/server/domain/workout/controller/WorkOutController.java
+++ backend/server/src/main/java/capstone/server/domain/workout/controller/WorkOutController.java
@@ -3,16 +3,20 @@ package capstone.server.domain.workout.controller;
 import capstone.server.domain.login.dto.KaKaoAccountIdAndUserType;
 import capstone.server.domain.workout.dto.RegisterWorkOutRequest;
 import capstone.server.domain.workout.dto.WorkOutCategoryResponse;
+import capstone.server.domain.workout.dto.WorkOutRecordResponse;
 import capstone.server.domain.workout.service.WorkOutService;
 import capstone.server.global.dto.DefaultResponse;
 import capstone.server.utils.KaKaoUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -67,7 +71,28 @@ public class WorkOutController {
                               .build()
               );
     }
+  }
+
+  @GetMapping("/records")
+  public ResponseEntity<?> getAllWorkOutRecords(Authentication authentication,
+                                                @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM")LocalDate date){
 
 
+    try {
+      KaKaoAccountIdAndUserType kaKaoAccountIdAndUserType = KaKaoUtil.authConvertIdAndTypeDto(authentication);
+      List<WorkOutRecordResponse> allWorkOutRecords = workOutService.getAllWorkOutRecords(kaKaoAccountIdAndUserType);
+      return ResponseEntity.ok()
+              .body(allWorkOutRecords);
+
+    }catch (HttpClientErrorException e) {
+      return ResponseEntity.status(e.getStatusCode())
+              .body(
+                      DefaultResponse.builder()
+                              .success(false)
+                              .status(e.getStatusCode().value())
+                              .message(e.getMessage())
+                              .build()
+              );
+    }
   }
 }
