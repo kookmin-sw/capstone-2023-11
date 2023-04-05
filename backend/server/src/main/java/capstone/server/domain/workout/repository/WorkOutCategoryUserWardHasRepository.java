4cb1399fcbc9b20af5001c935d52912ff5b70a1d
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-06 00:51:09 +0900

    [feat] 특정월 운동력 조회 레포지토리
    findAllByUserWardAndCreatedAtBetweenOrderByCreatedAtDesc 조건으로

--- backend/server/src/main/java/capstone/server/domain/workout/repository/WorkOutCategoryUserWardHasRepository.java
+++ backend/server/src/main/java/capstone/server/domain/workout/repository/WorkOutCategoryUserWardHasRepository.java
@@ -3,12 +3,19 @@ package capstone.server.domain.workout.repository;
 import capstone.server.entity.UserWard;
 import capstone.server.entity.WorkOutUserWardHas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 
 public interface WorkOutCategoryUserWardHasRepository extends JpaRepository<WorkOutUserWardHas,Long> {
 
   List<WorkOutUserWardHas> findAllByUserWardOrderByCreatedAtDesc(UserWard userWard);
+
+  List<WorkOutUserWardHas> findAllByUserWardAndCreatedAtBetweenOrderByCreatedAtDesc(UserWard userWard, LocalDateTime startDate, LocalDateTime lastDate);
+
 
 }
