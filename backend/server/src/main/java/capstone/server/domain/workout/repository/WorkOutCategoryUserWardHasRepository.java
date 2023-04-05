ba2d0006fdae0368a2e06c1d05c1d90317ffba98
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-04-05 23:29:07 +0900

    [feat] 유저를 기반으로 운동력 조회 레포지토리 구현

--- backend/server/src/main/java/capstone/server/domain/workout/repository/WorkOutCategoryUserWardHasRepository.java
+++ backend/server/src/main/java/capstone/server/domain/workout/repository/WorkOutCategoryUserWardHasRepository.java
@@ -1,9 +1,14 @@
 package capstone.server.domain.workout.repository;
 
+import capstone.server.entity.UserWard;
 import capstone.server.entity.WorkOutUserWardHas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface WorkOutCategoryUserWardHasRepository extends JpaRepository<WorkOutUserWardHas,Long> {
+
+  List<WorkOutUserWardHas> findAllByUserWardOrderByCreatedAtDesc(UserWard userWard);
 
 }
