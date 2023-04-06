1f078df3b5bca2420c829e3d8877f43df8fd5520
CULRRY <rkaghwns8@naver.com>
2023-04-06 15:44:49 +0900

    [feat] s3로 사진 업로드하는 유틸 구현

--- backend/server/src/main/java/capstone/server/config/S3Config.java
+++ backend/server/src/main/java/capstone/server/config/S3Config.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,7 +18,7 @@ public class S3Config {
 
     @Value("${cloud.aws.region.static}")
     private String region;
-
+    @Bean
     public AmazonS3 amazonS3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
---
+++ backend/server/src/main/java/capstone/server/utils/S3Util.java
@@ -0,0 +1,33 @@
+package capstone.server.utils;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3Util {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3Client amazonS3Client;
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getInputStream().available());
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), objectMetadata);
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+}
