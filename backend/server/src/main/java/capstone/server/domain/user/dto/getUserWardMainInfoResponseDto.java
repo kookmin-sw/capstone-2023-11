17d28829a50158faad8a9367c75de41e13c93826
CULRRY <rkaghwns8@naver.com>
2023-04-16 16:45:22 +0900

    [feat] getUserWardMainInfoResponseDto 작성

---
+++ backend/server/src/main/java/capstone/server/domain/user/dto/getUserWardMainInfoResponseDto.java
@@ -0,0 +1,20 @@
+package capstone.server.domain.user.dto;
+
+import capstone.server.domain.medicine.dto.ResponseMedicineInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class getUserWardMainInfoResponseDto {
+    private Long userCode;
+    private List<ResponseMedicineInfo> medicineInfoList;
+    private int todayMealCount;
+    private int todayWorkoutCount;
+}
