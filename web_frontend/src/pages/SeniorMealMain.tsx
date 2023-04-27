22ebb7a01b61bd39eeddd4e966788b6e3ccf4033
hayoun <fishingest@kookmin.ac.kr>
2023-04-27 15:02:50 +0900

    [feat]React Calendar 컴포넌트 구성

--- web_frontend/src/pages/SeniorMealMain.tsx
+++ web_frontend/src/pages/SeniorMealMain.tsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
+import BackButton from "../components/common/BackButton";
+import SeniorCalendar from "../components/common/SeniorCalendar";
+import { useState } from "react";
+import moment from "moment";
 
 function SeniorMealMain() {
-  return <StSeniorMealMain></StSeniorMealMain>;
+  const [selectedDate, setSelectedDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+  return (
+    <StSeniorMealMain>
+      <StHeader>
+        <BackButton />
+        <StTitle>식단 기록</StTitle>
+      </StHeader>
+      <SeniorCalendar setDate={setSelectedDate} />
+      <StDate className="date">{moment(selectedDate).format("YYYY년 MM월 DD일")}</StDate>
+      <StFoodContainer></StFoodContainer>
+    </StSeniorMealMain>
+  );
 }
 
 export default SeniorMealMain;
@@ -10,4 +25,26 @@ const StSeniorMealMain = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+const StTitle = styled.p`
+  width: 100%;
+  font-size: 3rem;
+  font-family: "Pretendard-Bold";
+  text-align: center;
+  padding-right: 2.5rem;
+`;
+const StHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.6rem;
+  width: 100%;
+`;
+const StDate = styled.p`
+  font-size: 2rem;
+  font-family: "Pretendard-Bold";
+  border-bottom: 0rem solid #ffffff;
+`;
+const StFoodContainer = styled.div`
+  overflow: scroll;
 `;
