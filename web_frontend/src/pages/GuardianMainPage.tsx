4133a6e57c213969ff278b33650272445abd1c72
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2023-05-01 15:28:04 +0900

    [init] 초기 설정

--- web_frontend/src/pages/GuardianMainPage.tsx
+++ web_frontend/src/pages/GuardianMainPage.tsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
 function GuardianMain() {
-  return null;
+  return (
+    <StContainer>
+      <StTitle></StTitle>
+    </StContainer>
+  );
 }
 
 export default GuardianMain;
+
+const StContainer = styled.div`
+  padding: 1rem 2rem;
+  justify-content: center;
+  margin: auto;
+`;
+
+const StTitle = styled.div`
+  font-family: "Pretendard-Bold";
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 0rem;
+  align-self: center;
+  padding-bottom: 1rem;
+`;
