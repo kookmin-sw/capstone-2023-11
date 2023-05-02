351d2a9d6a9f39fa43ea261a6b697fb0fb142696
CULRRY <rkaghwns8@naver.com>
2023-05-02 15:17:18 +0900

    [fix] dailySummaryDto 이름수정

--- backend/server/src/main/java/capstone/server/domain/user/dto/GetDailySummaryDto.java
+++ backend/server/src/main/java/capstone/server/domain/user/dto/GetDailySummaryDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DailySummaryDto {
+public class GetDailySummaryDto {
     List<MealInfo> meal;
     List<ExerciseInfo> exercise;
 }
