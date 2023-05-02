ca15ccec2f12d41e71145fb69d3f0974d177a0ab
CULRRY <rkaghwns8@naver.com>
2023-05-02 14:31:13 +0900

    [feat] daily summary dto 작성

---
+++ backend/server/src/main/java/capstone/server/domain/user/dto/ExerciseInfo.java
@@ -0,0 +1,13 @@
+package capstone.server.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExerciseInfo {
+}
