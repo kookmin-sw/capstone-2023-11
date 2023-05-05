603e1e4b28338fff050dd6483fa8e1edd30b0ee1
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-05-06 04:18:53 +0900

    [fix] properties 경로 재설정, deploy시 같이 복사하기

--- backend/server/scripts/start.sh
+++ backend/server/scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ec2-user/app/deploy"
 JAR_FILE="$PROJECT_ROOT/server-0.0.1-SNAPSHOT.jar"
+PROPERTIES_PATH = "$PROJECT_ROOT/application.properties"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -9,9 +10,9 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+# build 파일 복사 -> 이미 빌드된 *.jar 파일이 넘어오기때문에 불필요
+#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일에 실행권한 추가
 echo "> $JAR_FILE 에 실행권한 추가"
@@ -19,10 +20,7 @@ chmod +x $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties \
-  -Dspring.profiles.active=real \
-  $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Dspring.config.location=$PROPERTIES_PATH $JAR_FILE > $APP_LOG 2 > $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
