03a42752edc422d18517311d03b91e90bf38d481
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-05-06 03:53:18 +0900

    [feat] properties 설정 적용 및 chomod로 권한 획득

--- backend/server/scripts/start.sh
+++ backend/server/scripts/start.sh
@@ -13,9 +13,16 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
+# jar 파일에 실행권한 추가
+echo "> $JAR_FILE 에 실행권한 추가"
+chmod +x $JAR_FILE
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties \
+  -Dspring.profiles.active=real \
+  $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
