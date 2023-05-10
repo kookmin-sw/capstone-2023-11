0bd36ac924b6021549c6cef53595ce30078e8ff6
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-05-11 02:55:38 +0900

    [feat] 레포에서 UserWard로 UserGuardianUserWard 찾기

--- backend/server/src/main/java/capstone/server/domain/user/repository/UserGuardianUserWardRepository.java
+++ backend/server/src/main/java/capstone/server/domain/user/repository/UserGuardianUserWardRepository.java
@@ -1,9 +1,14 @@
 package capstone.server.domain.user.repository;
 
+import capstone.server.entity.UserGuardian;
 import capstone.server.entity.UserGuardianUserWard;
+import capstone.server.entity.UserWard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserGuardianUserWardRepository extends JpaRepository<UserGuardianUserWard,Long> {
+  List<UserGuardianUserWard> findUserGuardianUserWardsByUserWard(UserWard userWard);
 }
