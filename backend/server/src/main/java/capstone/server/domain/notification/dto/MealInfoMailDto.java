44c10ed73ebcf871429cd47439beec8ff7eb2f82
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-05-11 03:10:59 +0900

    [feat] 음식 등록시 sendFoodMail에 보내야하는 DTO 구현

---
+++ backend/server/src/main/java/capstone/server/domain/notification/dto/MealInfoMailDto.java
@@ -0,0 +1,17 @@
+package capstone.server.domain.notification.dto;
+
+import capstone.server.domain.food.dto.FoodInfo;
+import capstone.server.domain.food.dto.MealInfo;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class MealInfoMailDto {
+	private MealInfo mealInfo;
+	private List<FoodInfo> foodInfos;
+	private String imageUrl;
+	private Long wardId;
+}
