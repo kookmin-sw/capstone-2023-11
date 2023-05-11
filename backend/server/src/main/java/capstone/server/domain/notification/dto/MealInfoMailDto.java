667b9387eb87f78aaec2967cdea19599696bae72
CULRRY <rkaghwns8@naver.com>
2023-05-12 00:19:20 +0900

    [fix] MealInfoMailDto에 MealInfo가 아니라 독립적인 DTO로 구성

--- backend/server/src/main/java/capstone/server/domain/notification/dto/MealInfoMailDto.java
+++ backend/server/src/main/java/capstone/server/domain/notification/dto/MealInfoMailDto.java
@@ -2,14 +2,22 @@ package capstone.server.domain.notification.dto;
 
 import capstone.server.domain.food.dto.FoodInfo;
 import capstone.server.domain.food.dto.MealInfo;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MealInfoMailDto {
-	private MealInfo mealInfo;
+	private LocalDateTime dateTime;
+	private int times;
+	private String imageUrl;
+	private List<FoodInfo> details;
 	private Long wardId;
 }
