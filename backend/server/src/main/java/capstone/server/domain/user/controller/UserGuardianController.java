3d1bb30f210120968c4f4ab0c1e5cb4c6cd6289e
CULRRY <rkaghwns8@naver.com>
2023-05-12 22:23:40 +0900

    [feat] 가디언에서 와드 Id로 해당기능 가능해게 구현

---
+++ backend/server/src/main/java/capstone/server/domain/user/controller/UserGuardianController.java
@@ -0,0 +1,73 @@
+package capstone.server.domain.user.controller;
+
+import capstone.server.domain.user.dto.GetDailySummaryDto;
+import capstone.server.domain.user.dto.GetUserWardMainInfoResponseDto;
+import capstone.server.domain.user.dto.GetWeeklySummaryDto;
+import capstone.server.domain.user.service.UserGuardianService;
+import capstone.server.global.dto.DefaultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/userguardian")
+public class UserGuardianController {
+    private final UserGuardianService userGuardianService;
+
+    @GetMapping(value = "/main")
+    public ResponseEntity<?> getUserWardMainInfo(Authentication authentication, @RequestParam(value = "wardId") Long userWardKakaoAccountId) {
+        try {
+            GetUserWardMainInfoResponseDto result = userGuardianService.getUserWardMainInfo(userWardKakaoAccountId);
+            return ResponseEntity.ok().body(result);
+
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(
+                    DefaultResponse.builder()
+                            .success(false)
+                            .message(e.getResponseBodyAsString())
+                            .status(500)
+                            .build()
+            );
+        }
+    }
+
+    @GetMapping(value = "/summary/daily")
+    public ResponseEntity<?> getDailySummary(Authentication authentication, @RequestParam(value = "wardId") Long userWardKakaoAccountId) {
+        try {
+            GetDailySummaryDto result = userGuardianService.getDailySummary(userWardKakaoAccountId);
+            return ResponseEntity.ok().body(result);
+
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(
+                    DefaultResponse.builder()
+                            .success(false)
+                            .message(e.getResponseBodyAsString())
+                            .status(500)
+                            .build()
+            );
+        }
+    }
+
+    @GetMapping(value = "/summary/weekly")
+    public ResponseEntity<?> getWeeklySummary(Authentication authentication, @RequestParam(value = "wardId") Long userWardKakaoAccountId) {
+        try {
+            GetWeeklySummaryDto result = userGuardianService.getWeeklySummary(userWardKakaoAccountId);
+            return ResponseEntity.ok().body(result);
+
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(
+                    DefaultResponse.builder()
+                            .success(false)
+                            .message(e.getResponseBodyAsString())
+                            .status(500)
+                            .build()
+            );
+        }
+    }
+}
