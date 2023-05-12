c9a40f51ac894d37bf496e0c8d501e10fb8265aa
동재 <59719500+jea0716@users.noreply.github.com>
2023-05-12 19:49:20 +0900

    [feat] user 음식, 운동 데이터 추가 구현

--- web_frontend/src/components/seniorSummary/Advice.tsx
+++ web_frontend/src/components/seniorSummary/Advice.tsx
@@ -7,28 +7,54 @@ import { IWeeklyData, nameAtom } from "../../core/atom";
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
-export function SeniorAdvice(data: IWeeklyData) {
+export function SeniorAdvice(prop: IWeeklyData) {
+  const [firstAPI, setFirstAPI] = useState(false);
   const getNameAtom = useRecoilValue(nameAtom);
   const exerCalories = [];
-  if (data) {
+  const foodCalories = [];
+  const amountProtein = [];
+  const amountFat = [];
+  const amountCarbohy = [];
+  const [answer, setAnswer] = useState("");
+
+  if (prop) {
     for (let i = 0; i < 7; i++) {
-      const calData = data.weeklyExerciseInfo[i].calorie;
+      const calData = prop.weeklyExerciseInfo[i].calorie;
+      const calories = Math.round(prop.weeklyFoodNutrientSum[i].calorie);
+      const fat = Math.round(prop.weeklyFoodNutrientSum[i].fat);
+      const carbohyrate = Math.round(prop.weeklyFoodNutrientSum[i].carbohydrate);
+      const protein = Math.round(prop.weeklyFoodNutrientSum[i].protein);
+
+      foodCalories.push(calories);
       exerCalories.push(calData);
+      amountFat.push(fat);
+      amountCarbohy.push(carbohyrate);
+      amountProtein.push(protein);
     }
   }
-  console.log(exerCalories);
-
   const countExercise = exerCalories.filter((num) => num !== 0).length;
+  const ate = foodCalories.filter((num) => num !== 0).length;
+  const totalCal = foodCalories.reduce(function add(sum, currValue) {
+    return sum + currValue;
+  }, 0);
+  const totalFat = amountFat.reduce(function add(sum, currValue) {
+    return sum + currValue;
+  }, 0);
+  const totalProtein = amountProtein.reduce(function add(sum, currValue) {
+    return sum + currValue;
+  }, 0);
+  const totalCarbohy = amountCarbohy.reduce(function add(sum, currValue) {
+    return sum + currValue;
+  }, 0);
 
-  const [answer, setAnswer] = useState("");
-  console.log(data);
   useEffect(() => {
-    callOpenAIApi();
-  }, [data]);
+    if (prop && firstAPI == false) {
+      setFirstAPI(true);
+      callOpenAIApi();
+    }
+  }, [[prop]]);
 
   async function callOpenAIApi() {
-    console.log("calling api...");
-
     const APIBody = {
       model: "text-davinci-003",
       prompt: `I am a highly intelligent question answering bot. 
@@ -36,14 +62,18 @@ export function SeniorAdvice(data: IWeeklyData) {
       I will give you the answer to Korean. 
       If you ask me a question that is nonsense, trickery, or has no clear answer, 
       I will respond with "잘 모르겠어요.".
-      Q: 이사람의 이름은 ${getNameAtom}이고, ${data.age}세, ${data.height}cm, ${data.weight}kg ${
-        data.gender == "MALE" ? "남성" : "여성"
+      Q: 이사람의 이름은 ${getNameAtom}이고, ${prop.age}세, ${prop.height}cm, ${prop.weight}kg ${
+        prop.gender == "MALE" ? "남성" : "여성"
       }입니다. 
+      이 사람의 7일간 식사데이터를 알려드리겠습니다. 일일 평균 칼로리 섭취량 ${totalCal / ate},
+      평균 지방 섭취량 ${totalFat / ate}, 평균 단백질 섭취량 ${totalProtein / ate}, 평균 탄수화물 섭취량 ${
+        totalCarbohy / ate
+      }입니다.
       이 사람은 7일간 총 ${countExercise}일 운동했습니다.
       이 사람의 한줄평 및 추천하는 식사의 예, 추천하는 운동을 아래의 예시처럼 보여주세요.
-      "${data.name}의 종합평가는 같은 연령대${
-        data.age
-      }에 비해 ~~합니다.\n ~~와 같은 이유 때문에 ~~와 같은 음식을 추천드립니다.\n ~~와 같은 이유 때문에 ~~와 같은 운동을 추천드립니다." A:`,
+      "${prop.name}님의 종합평가는 같은 연령대${
+        prop.age
+      }에 비해 ~~합니다.<br> ~~와 같은 이유 때문에 ~~와 같은 음식을 추천드립니다.<br> ~~와 같은 이유 때문에 ~~와 같은 운동을 추천드립니다." A:`,
       temperature: 0,
       max_tokens: 2000,
       top_p: 1,
