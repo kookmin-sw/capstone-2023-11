3fd88c8d7fb4621d9d2e7b846d241ea7b785e23c
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2023-05-15 20:03:45 +0900

    [fix] 운동 소모 칼로리 수정

--- backend/server/src/main/java/capstone/server/domain/workout/enums/WorkOutCategoryEnum.java
+++ backend/server/src/main/java/capstone/server/domain/workout/enums/WorkOutCategoryEnum.java
@@ -6,24 +6,24 @@ import lombok.Getter;
 @Getter
 public enum WorkOutCategoryEnum {
 
-  WALKING("walking","걷기",300,"걷기에 대한 설명"),
-  RUNNING("running","달리기",840,"달리기에 대한 설명"),
-  YOGA("yoga","요가",300,"요가에 대한 설명"),
-  PILATES("pilates","필라테스",420,"필라테스에 대한 설명"),
-  GATEBALL("gateball","게이트볼",400,"게이트볼에 대한 설명"),
-  BICYCLING("bicycling","자전거",480,"자전거에 대한 설명"),
-  HIKING("hiking","등산",480,"등산에 대한 설명"),
-  GOLF("golf","골프",250,"골프에 대한 설명"),
-  DANCE_SPORTS("dance sports","댄스 스포츠",360,"댄스 스포츠에 대한 설명"),
-  WEIGHTLIFTING("weightlifting","웨이트",324,"웨이트에 대한 설명"),
-  TENNIS("tennis","테니스",480,"테니스에 대한 설명"),
-  BADMINTON("badminton","배드민턴",420,"배드민턴에 대한 설명"),
-  SQUASH("squash","스쿼시",408,"스쿼시에 대한 설명"),
-  BOXING("boxing","복싱",400,"복싱에 대한 설명"),
-  STRETCHING("stretching","스트레칭",360,"스트레칭에 대한 설명"),
-  AEROBICS("aerobics","에어로빅",840,"에어로빅에 대한 설명"),
-  RECREATION("recreation","당구, 탁구",200,"레크리에이션(당구, 탁구)에 대한 설명"),
-  SURFING("surfing","서핑",306,"서핑에 대한 설명");
+  WALKING("walking","걷기",240,"걷기에 대한 설명"),
+  RUNNING("running","달리기",650,"달리기에 대한 설명"),
+  YOGA("yoga","요가",240,"요가에 대한 설명"),
+  PILATES("pilates","필라테스",240,"필라테스에 대한 설명"),
+  GATEBALL("gateball","게이트볼",300,"게이트볼에 대한 설명"),
+  BICYCLING("bicycling","자전거",400,"자전거에 대한 설명"),
+  HIKING("hiking","등산",450,"등산에 대한 설명"),
+  GOLF("golf","골프",300,"골프에 대한 설명"),
+  DANCE_SPORTS("dance sports","댄스 스포츠",450,"댄스 스포츠에 대한 설명"),
+  WEIGHTLIFTING("weightlifting","웨이트",300,"웨이트에 대한 설명"),
+  TENNIS("tennis","테니스",450,"테니스에 대한 설명"),
+  BADMINTON("badminton","배드민턴",370,"배드민턴에 대한 설명"),
+  SQUASH("squash","스쿼시",650,"스쿼시에 대한 설명"),
+  BOXING("boxing","복싱",700,"복싱에 대한 설명"),
+  STRETCHING("stretching","스트레칭",80,"스트레칭에 대한 설명"),
+  AEROBICS("aerobics","에어로빅",450,"에어로빅에 대한 설명"),
+  RECREATION("recreation","당구, 탁구",120,"레크리에이션(당구, 탁구)에 대한 설명"),
+  SURFING("surfing","서핑",170,"서핑에 대한 설명");
 
 
   private String eng;
