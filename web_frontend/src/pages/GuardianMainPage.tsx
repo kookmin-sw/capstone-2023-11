2027591898ca6ca2b6db3b3b6fbdaff43ab8d099
hayoun <fishingest@kookmin.ac.kr>
2023-05-16 02:05:27 +0900

    [feat] Swiper 스타일링

--- web_frontend/src/pages/GuardianMainPage.tsx
+++ web_frontend/src/pages/GuardianMainPage.tsx
@@ -9,10 +9,12 @@ import "swiper/css/pagination";
 
 // import required modules
 import { EffectCoverflow, Pagination } from "swiper";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <>
+    <StGuardianMainPage>
+      <StTitle>관리중인 시니어</StTitle>
       <Swiper
         effect={"coverflow"}
         grabCursor={true}
@@ -29,7 +31,7 @@ export default function App() {
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper">
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <StSeniorCard></StSeniorCard>
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
@@ -56,6 +58,37 @@ export default function App() {
           <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
         </SwiperSlide>
       </Swiper>
-    </>
+    </StGuardianMainPage>
   );
 }
+
+const StGuardianMainPage = styled.div`
+  .swiper {
+    width: 30rem;
+    height: 55rem;
+    border-radius: 1rem;
+  }
+  img {
+    width: 30rem;
+    height: 55rem;
+    border-radius: 1rem;
+  }
+  .swiper-slide {
+    margin-top: 3rem;
+    border-radius: 1rem;
+  }
+`;
+
+const StSeniorCard = styled.div`
+  background-color: #f8f9fe;
+  width: 30rem;
+  height: 55rem;
+  border-radius: 2rem;
+`;
+const StTitle = styled.div`
+  font-family: "Pretendard-Bold";
+  font-size: 2.3rem;
+  text-align: center;
+  align-self: center;
+  margin-top: 2rem;
+`;
